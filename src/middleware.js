961355beb1b17db9c0a425bc091cc64720d2153c
Mitch Robb <olordhux+github@gmail.com>
2017-02-05 01:00:17 -0800

    Middleware must not return promise on actions without [CALL_API]
    https://github.com/agraboso/redux-api-middleware/pull/80

--- src/middleware.js
+++ src/middleware.js
@@ -21,160 +21,162 @@ function apiMiddleware(storeOrFetch) {
   }
   const { getState } = storeOrFetch;
 
-  return (next) => async (action) => {
+  return (next) => (action) => {
     // Do not process actions without a [CALL_API] property
     if (!isRSAA(action)) {
       return next(action);
     }
 
-    // Try to dispatch an error request FSA for invalid RSAAs
-    const validationErrors = validateRSAA(action);
-    if (validationErrors.length) {
-      const callAPI = action[CALL_API];
-      if (callAPI.types && Array.isArray(callAPI.types)) {
-        let requestType = callAPI.types[0];
-        if (requestType && requestType.type) {
-          requestType = requestType.type;
+    return (async () => {
+      // Try to dispatch an error request FSA for invalid RSAAs
+      const validationErrors = validateRSAA(action);
+      if (validationErrors.length) {
+        const callAPI = action[CALL_API];
+        if (callAPI.types && Array.isArray(callAPI.types)) {
+          let requestType = callAPI.types[0];
+          if (requestType && requestType.type) {
+            requestType = requestType.type;
+          }
+          next({
+            type: requestType,
+            payload: new InvalidRSAA(validationErrors),
+            error: true
+          });
         }
-        next({
-          type: requestType,
-          payload: new InvalidRSAA(validationErrors),
-          error: true
-        });
-      }
-      return;
-    }
-
-    // Parse the validated RSAA action
-    const callAPI = action[CALL_API];
-    var { endpoint, body, headers, options = {} } = callAPI;
-    const { method, credentials, bailout, types } = callAPI;
-    const [requestType, successType, failureType] = normalizeTypeDescriptors(types);
-
-    // Should we bail out?
-    try {
-      if ((typeof bailout === 'boolean' && bailout) ||
-          (typeof bailout === 'function' && bailout(getState()))) {
         return;
       }
-    } catch (e) {
-      return next(await actionWith(
-        {
-          ...requestType,
-          payload: new RequestError('[CALL_API].bailout function failed'),
-          error: true
-        },
-        [action, getState()]
-      ));
-    }
 
-    // Process [CALL_API].endpoint function
-    if (typeof endpoint === 'function') {
+      // Parse the validated RSAA action
+      const callAPI = action[CALL_API];
+      var { endpoint, body, headers, options = {} } = callAPI;
+      const { method, credentials, bailout, types } = callAPI;
+      const [requestType, successType, failureType] = normalizeTypeDescriptors(types);
+
+      // Should we bail out?
       try {
-        endpoint = endpoint(getState());
+        if ((typeof bailout === 'boolean' && bailout) ||
+          (typeof bailout === 'function' && bailout(getState()))) {
+          return;
+        }
       } catch (e) {
         return next(await actionWith(
           {
             ...requestType,
-            payload: new RequestError('[CALL_API].endpoint function failed'),
+            payload: new RequestError('[CALL_API].bailout function failed'),
             error: true
           },
           [action, getState()]
         ));
       }
-    }
 
-    // Process [CALL_API].body function
-    if (typeof body === 'function') {
+      // Process [CALL_API].endpoint function
+      if (typeof endpoint === 'function') {
+        try {
+          endpoint = endpoint(getState());
+        } catch (e) {
+          return next(await actionWith(
+            {
+              ...requestType,
+              payload: new RequestError('[CALL_API].endpoint function failed'),
+              error: true
+            },
+            [action, getState()]
+          ));
+        }
+      }
+
+      // Process [CALL_API].body function
+      if (typeof body === 'function') {
+        try {
+          body = body(getState());
+        } catch (e) {
+          return next(await actionWith(
+            {
+              ...requestType,
+              payload: new RequestError('[CALL_API].body function failed'),
+              error: true
+            },
+            [action, getState()]
+          ));
+        }
+      }
+
+      // Process [CALL_API].headers function
+      if (typeof headers === 'function') {
+        try {
+          headers = headers(getState());
+        } catch (e) {
+          return next(await actionWith(
+            {
+              ...requestType,
+              payload: new RequestError('[CALL_API].headers function failed'),
+              error: true
+            },
+            [action, getState()]
+          ));
+        }
+      }
+
+      // Process [CALL_API].options function
+      if (typeof options === 'function') {
+        try {
+          options = options(getState());
+        } catch (e) {
+          return next(await actionWith(
+            {
+              ...requestType,
+              payload: new RequestError('[CALL_API].options function failed'),
+              error: true
+            },
+            [action, getState()]
+          ));
+        }
+      }
+
+      // We can now dispatch the request FSA
+      if (typeof requestType.payload === 'function' || typeof requestType.meta === 'function') {
+        next(await actionWith(
+          requestType,
+          [action, getState()]
+        ));
+      } else {
+        next(requestType);
+      }
+
       try {
-        body = body(getState());
+        // Make the API call
+        var res = await fetch(endpoint, {
+          ...options,
+          method, body, credentials, headers
+        });
       } catch (e) {
+        // The request was malformed, or there was a network error
         return next(await actionWith(
           {
             ...requestType,
-            payload: new RequestError('[CALL_API].body function failed'),
+            payload: new RequestError(e.message),
             error: true
           },
           [action, getState()]
         ));
       }
-    }
 
-    // Process [CALL_API].headers function
-    if (typeof headers === 'function') {
-      try {
-        headers = headers(getState());
-      } catch (e) {
+      // Process the server response
+      if (res.ok) {
+        return next(await actionWith(
+          successType,
+          [action, getState(), res]
+        ));
+      } else {
         return next(await actionWith(
           {
-            ...requestType,
-            payload: new RequestError('[CALL_API].headers function failed'),
+            ...failureType,
             error: true
           },
-          [action, getState()]
+          [action, getState(), res]
         ));
       }
-    }
-
-    // Process [CALL_API].options function
-    if (typeof options === 'function') {
-      try {
-        options = options(getState());
-      } catch (e) {
-        return next(await actionWith(
-          {
-            ...requestType,
-            payload: new RequestError('[CALL_API].options function failed'),
-            error: true
-          },
-          [action, getState()]
-        ));
-      }
-    }
-
-    // We can now dispatch the request FSA
-    if(typeof requestType.payload === 'function' || typeof requestType.meta === 'function') {
-      next(await actionWith(
-        requestType,
-        [action, getState()]
-      ));
-    } else {
-      next(requestType);
-    }
-
-    try {
-      // Make the API call
-      var res = await fetch(endpoint, {
-        ...options,
-        method, body, credentials, headers
-      });
-    } catch(e) {
-      // The request was malformed, or there was a network error
-      return next(await actionWith(
-        {
-          ...requestType,
-          payload: new RequestError(e.message),
-          error: true
-        },
-        [action, getState()]
-      ));
-    }
-
-    // Process the server response
-    if (res.ok) {
-      return next(await actionWith(
-        successType,
-        [action, getState(), res]
-      ));
-    } else {
-      return next(await actionWith(
-        {
-          ...failureType,
-          error: true
-        },
-        [action, getState(), res]
-      ));
-    }
+    })();
   }
 }
 
